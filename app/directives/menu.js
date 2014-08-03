6e1a78812de586cc39c193f027ba6b581a753a6c
Liz Abinante <eabinante@gmail.com>
2014-08-03 15:48:48 -0500

    starting angular outline with menu directive

---
+++ app/directives/menu.js
@@ -0,0 +1,19 @@
+(function(){
+  var app = angular.module("gifParty", []);
+
+  app.directive("menu", ['$http', function($http) {
+    return {
+      restrict: "E",
+      templateUrl: "/app/views/menu.html",
+      controller: function() {
+        var menu = this;
+        menu.items = [];
+
+        $http.get('/app/items.json').success(function(data) {
+          menu.items = data.items;
+        });
+      },
+      controllerAs: "menu"
+    }
+  }]);
+})();
