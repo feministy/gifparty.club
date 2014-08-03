5a96c4b74cb2271f22b1f8d7b7c4b8a7c666ed69
Liz Abinante <eabinante@gmail.com>
2014-08-03 18:00:51 -0500

    all routes

--- app/app.js
+++ app/app.js
@@ -12,4 +12,69 @@ gifPartyApp.config(["$routeProvider",
         controller: "AngryController",
         controllerAs: "ctrl"
       })
+      .when("/annoyed", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "AnnoyedController",
+        controllerAs: "ctrl"
+      })
+      .when("/comforting", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "ComfortingController",
+        controllerAs: "ctrl"
+      })
+      .when("/cute", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "CuteController",
+        controllerAs: "ctrl"
+      })
+      .when("/dancing", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "DancingController",
+        controllerAs: "ctrl"
+      })
+      .when("/evil", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "EvilController",
+        controllerAs: "ctrl"
+      })
+      .when("/fuck_you", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "FuckYouController",
+        controllerAs: "ctrl"
+      })
+      .when("/funny", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "FunnyController",
+        controllerAs: "ctrl"
+      })
+      .when("/happy", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "HappyController",
+        controllerAs: "ctrl"
+      })
+      .when("/nope", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "NopeController",
+        controllerAs: "ctrl"
+      })
+      .when("/sad", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "SadController",
+        controllerAs: "ctrl"
+      })
+      .when("/victory", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "VictoryController",
+        controllerAs: "ctrl"
+      })
+      .when("/whatever", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "WhateverController",
+        controllerAs: "ctrl"
+      })
+      .when("/wtf", {
+        templateUrl: "/app/views/gifs.html",
+        controller: "WtfController",
+        controllerAs: "ctrl"
+      })
   }]);
