3fdf39ca48c84b92929ecdd55ba5c0d0011e268a
Liz Abinante <eabinante@gmail.com>
2015-01-17 12:57:09 -0800

    is_owner? and #current_user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= session[:current_user_id] && User.find(session[:current_user_id])
+  end
+
+  def is_owner?
+    current_user.id == @user.id
+  end
 end
