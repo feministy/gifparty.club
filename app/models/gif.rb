ddd2afb14739962f5d537bcfe18d613fde65d7fd
Liz Abinante <eabinante@gmail.com>
2015-01-18 19:51:28 -0800

    clean up paperclip validations

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,16 +1,11 @@
 class Gif < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
-
   has_attached_file :image,
-                    :storange => :s3,
-                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+                    :storage => :s3,
+                    :s3_credentials => {bucket: "gifpartyclub", access_key_id: ENV['AWS_GIFPARTY_PAPERCLIP_ACCESS'], secret_access_key: ENV['AWS_GIFPARTY_PAPERCLIP_SECRET']}
 
-  def s3_credentials
-    {
-      bucket: "gifpartyclub",
-      access_key_id: ENV['AWS_GIFPARTY_PAPERCLIP_ACCESS'],
-      secret_access_key: ENV['AWS_GIFPARTY_PAPERCLIP_SECRET']
-    }
-  end
+  validates_presence_of :user, :category
+  validates :image, :attachment_presence => true
+  validates_attachment_content_type :image, :content_type => ["image/gif"]
 end
