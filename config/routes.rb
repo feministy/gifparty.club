dd6ee3330a344336aa78dfcd2af19654fa2670ed
Liz Abinante <eabinante@gmail.com>
2015-01-18 20:04:04 -0800

    reorder

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :gifs, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: :show
   resources :categories, only: [:index]
-  resources :gifs, only: [:new, :create]
 
   # custom route for fancy path
   get ':id' => 'categories#show', as: 'category'
